6ac4f85a25fdd0afdcf3ba6053cbe32ad81c0838
Nguyễn Bảo Ngọc <141977695+ngocjday04@users.noreply.github.com>
2025-06-25 12:12:08 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapperMVC.Mapping;
+using AutoMapperMVC.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AutoMapperMVC.Data;
+
+
+namespace TodoListMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddAutoMapper(typeof(MappingProfile));
+            builder.Services.AddDbContext<AppDbContext>(options =>
+     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+
+            // Đăng ký Repository
+            builder.Services.AddSingleton<TodoRepository>();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
